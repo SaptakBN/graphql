652f765d4aef76867221cd98e5c67a24f09e32e6
Saptak Banerjee <saptak@pitangent.com>
2025-01-28 18:42:42 +0530

    feat: home page added

--- client/src/pages/authorized/home.tsx
+++ client/src/pages/authorized/home.tsx
@@ -1,4 +1,46 @@
+import "./home.css";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <main className="main">
+      <div className="blog-container">
+        <div className="blog-header">
+          <div className="blog-author--no-cover">
+            <h3>Russ Beye</h3>
+          </div>
+        </div>
+
+        <div className="blog-body">
+          <div className="blog-title">
+            <h1>
+              <a href="#">This Post Has No Cover Image</a>
+            </h1>
+          </div>
+          <div className="blog-summary">
+            <p>
+              Here is an example of a post without a cover image. You don't always have to have a cover image. In fact, leaving them out from time to
+              time and disrupt the predictive flow and make the overall design more interesting. Something to think about.
+            </p>
+          </div>
+        </div>
+
+        <div className="blog-footer">
+          <ul>
+            <li className="published-date">12 days ago</li>
+            <li className="comments">
+              <a href="#">
+                <span className="numero">8</span>
+              </a>
+            </li>
+            <li className="shares">
+              <a href="#">
+                <span className="numero">3</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
 };
 export default Home;
